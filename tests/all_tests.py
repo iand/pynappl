ca7affbb75fd3991b7e11df7de82414b2ac8a7e4
innovateer <innovateer@0d219f22-8b0e-11de-a676-0be261666330>
2009-08-17 14:18:19 +0000

    Added test for describe method

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -5,8 +5,9 @@ import store_test
 
 
 def suite():
-  suite1 = unittest.TestLoader().loadTestsFromTestCase(store_test.BuildUri)
-  return unittest.TestSuite([suite1])
+  suite1 = unittest.TestLoader().loadTestsFromTestCase(store_test.BuildUriTestCase)
+  suite2 = unittest.TestLoader().loadTestsFromTestCase(store_test.DescribeTestCase)
+  return unittest.TestSuite([suite1, suite2])
 
 
 if __name__ == '__main__':
--- tests/store_test.py
+++ tests/store_test.py
@@ -1,7 +1,9 @@
 import unittest
 import pynappl
+import urllib
+from mock_http import MockHttp
 
-class BuildUri(unittest.TestCase):
+class BuildUriTestCase(unittest.TestCase):
 
   def test_build_uri_abs(self):
     """build_uri returns absolute URIs if they are in store's URI space"""
@@ -18,6 +20,14 @@ class BuildUri(unittest.TestCase):
     store = pynappl.Store('http://example.com/store')
     self.assertEqual('http://example.com/store/foo', store.build_uri('foo'))
     
+
+class DescribeTestCase(unittest.TestCase):
+
+  def test_describe_single_uri_performs_get_on_metabox(self):
+    client = MockHttp()
+    store = pynappl.Store('http://example.com/store', client=client)
+    resp = store.describe('http://example.com/foo')
+    self.assertTrue(client.received_request('get', 'http://example.com/store/meta?about=' + urllib.quote_plus('http://example.com/foo')))
 
 
 if __name__ == "__main__":
