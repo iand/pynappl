664804181e36ba554e11b60157d31a7d7494277c
me@kierdavis.com <me@kierdavis.com@0d219f22-8b0e-11de-a676-0be261666330>
2010-03-08 20:14:53 +0000

    Removed EntityMatcher.lookup; store names can be specified in the Store constructor and will be appended to "http://api.talis.com/stores/"

--- src/pynappl/entitymatcher.py
+++ src/pynappl/entitymatcher.py
@@ -9,20 +9,20 @@ class EntityMatcher:
 		for name in names:
 			uris.append(self.lookup(name, type, default))
 		return uris
-	def lookup(self, name, type=None, default=None):
-		name = rdflib.term.Literal(name.capitalize())
-		if type is None:
-			rows = self.graph.query("SELECT ?uri WHERE {?uri rdfs:label ?name.}", initNs=dict(
-				rdfs=rdflib.namespace.Namespace("http://www.w3.org/2000/01/rdf-schema#"),
-			), initBindings=dict(name=name))
-		else:
-			type = rdflib.term.URIRef("http://example.com/entitymatcher/types/" + type.capitalize())
-			rows = self.graph.query("SELECT ?uri WHERE {?uri rdfs:label ?name. ?uri rdf:type ?type.}", initNs=dict(
-				rdfs=rdflib.namespace.Namespace("http://www.w3.org/2000/01/rdf-schema#"),
-				rdf=rdflib.namespace.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
-			), initBindings=dict(name=name, type=type))
-		l = list(rows)
-		if len(l):
-			return str(l[0])
-		else:
-			return default
+	#~ def lookup(self, name, type=None, default=None):
+		#~ name = rdflib.term.Literal(name.capitalize())
+		#~ if type is None:
+			#~ rows = self.graph.query("SELECT ?uri WHERE {?uri rdfs:label ?name.}", initNs=dict(
+				#~ rdfs=rdflib.namespace.Namespace("http://www.w3.org/2000/01/rdf-schema#"),
+			#~ ), initBindings=dict(name=name))
+		#~ else:
+			#~ type = rdflib.term.URIRef("http://example.com/entitymatcher/types/" + type.capitalize())
+			#~ rows = self.graph.query("SELECT ?uri WHERE {?uri rdfs:label ?name. ?uri rdf:type ?type.}", initNs=dict(
+				#~ rdfs=rdflib.namespace.Namespace("http://www.w3.org/2000/01/rdf-schema#"),
+				#~ rdf=rdflib.namespace.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
+			#~ ), initBindings=dict(name=name, type=type))
+		#~ l = list(rows)
+		#~ if len(l):
+			#~ return str(l[0])
+		#~ else:
+			#~ return default
