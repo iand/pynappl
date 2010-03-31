378ee613a934a80569a4c14409762ada1430ac85
me@kierdavis.com <me@kierdavis.com@0d219f22-8b0e-11de-a676-0be261666330>
2010-03-31 16:39:46 +0000

    Added bin/tconsole and src/changeset.py

---
+++ trunk/src/pynappl/changeset.py
@@ -0,0 +1,96 @@
+__all__ = ["ChangeSet", "BatchChangeSet"]
+
+import time
+import rdflib
+
+RDF = rdflib.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+CS = rdflib.Namespace("http://purl.org/vocab/changeset/schema#")
+
+class ChangeSet(rdflib.ConjunctiveGraph):
+	def __init__(self, subjectOfChange):
+		s = super(ChangeSet, self)
+		s.__init__()
+		self._add = s.add
+		self.subjectOfChange = subjectOfChange
+		self.subj = rdflib.BNode()
+		self.bind("rdf", RDF)
+		self.bind("cs", CS)
+		self._add((self.subj, RDF.type, CS.ChangeSet))
+		self._add((self.subj, CS.createdDate, rdflib.Literal(time.strftime('%Y-%m-%dT%H:%M:%SZ'))))
+		self._add((self.subj, CS.subjectOfChange, subjectOfChange))
+	
+	def setCreatorName(self, creatorName):
+		self._add((self.subj, CS.creatorName, rdflib.Literal(creatorName)))
+	
+	def setChangeReason(self, changeReason):
+		self._add((self.subj, CS.changeReason, rdflib.Literal(changeReason)))
+	
+	def add(self, pred, obj):
+		stmt = rdflib.BNode()
+		self._add((self.subj, CS.addition, stmt))
+		self._add((stmt, RDF.type, RDF.Statement))
+		self._add((stmt, RDF.subject, self.subjectOfChange))
+		self._add((stmt, RDF.predicate, pred))
+		self._add((stmt, RDF.object, obj))
+		return stmt
+	
+	def remove(self, pred, obj):
+		stmt = rdflib.BNode()
+		self._add((self.subj, CS.removal, stmt))
+		self._add((stmt, RDF.type, RDF.Statement))
+		self._add((stmt, RDF.subject, self.subjectOfChange))
+		self._add((stmt, RDF.predicate, pred))
+		self._add((stmt, RDF.object, obj))
+		return stmt
+
+class BatchChangeSet(object):
+	def __init__(self):
+		self.changesets = {}
+		self.creatorName = ""
+		self.changeReason = ""
+	
+	def setCreatorName(self, creatorName):
+		self.creatorName = creatorName
+	
+	def setChangeReason(self, changeReason):
+		self.changeReason = changeReason
+	
+	def add(self, s, p, o):
+		if s not in self.changesets:
+			cs = ChangeSet(s)
+			cs.setCreatorName(self.creatorName)
+			cs.setChangeReason(self.changeReason)
+			self.changesets[s] = cs
+		return self.changesets[s].add(p, o)
+	
+	def remove(self, s, p, o):
+		if s not in self.changesets:
+			cs = ChangeSet(s)
+			cs.setCreatorName(self.creatorName)
+			cs.setChangeReason(self.changeReason)
+			self.changesets[s] = cs
+		return self.changesets[s].remove(p, o)
+	
+	def getGraph(self):
+		g = rdflib.ConjunctiveGraph()
+		g.bind("rdf", RDF)
+		g.bind("cs", CS)
+		for cs in self.changesets.values():
+			g += cs
+		return g
+	
+	def getGraphs(self):
+		g = rdflib.ConjunctiveGraph()
+		g.bind("rdf", RDF)
+		g.bind("cs", CS)
+		i = 0
+		for cs in self.changesets.values():
+			g += cs
+			i += 1
+			if i >= 14:
+				yield g
+				g = rdflib.ConjunctiveGraph()
+				g.bind("rdf", RDF)
+				g.bind("cs", CS)
+		if len(g):
+			yield g
